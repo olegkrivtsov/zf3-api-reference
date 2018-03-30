a3e280c8343ebd9e6343f1a04be12b85092536ff
Oleg Krivtsov <olegkrivtsov@gmail.com>
2018-03-30 15:19:09 +0600

    Added new components

---
+++ data/upper_ad.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ZF3 API Reference - Upper Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3933778336056438"
+     data-ad-slot="7172748715"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
